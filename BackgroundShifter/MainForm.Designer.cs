557a6fd227e9b7fa11bebd4585a6a602fb8fbb53
Markus <Markus@DESKTOP-00FFVSH>
2024-05-02 01:31:25 +0300

    Add project files.

---
+++ BackgroundShifter/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace BackgroundShifter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveLink = new System.Windows.Forms.LinkLabel();
+            this.openLink = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.recalculateLink = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // saveLink
+            // 
+            this.saveLink.AutoSize = true;
+            this.saveLink.Location = new System.Drawing.Point(87, 284);
+            this.saveLink.Name = "saveLink";
+            this.saveLink.Size = new System.Drawing.Size(60, 13);
+            this.saveLink.TabIndex = 0;
+            this.saveLink.TabStop = true;
+            this.saveLink.Text = "Save result";
+            this.saveLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.saveLink_LinkClicked);
+            // 
+            // openLink
+            // 
+            this.openLink.AutoSize = true;
+            this.openLink.Location = new System.Drawing.Point(12, 284);
+            this.openLink.Name = "openLink";
+            this.openLink.Size = new System.Drawing.Size(69, 13);
+            this.openLink.TabIndex = 1;
+            this.openLink.TabStop = true;
+            this.openLink.Text = "Open original";
+            this.openLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.openLink_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Original image:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 107);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Processed image:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(12, 151);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 107);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // recalculateLink
+            // 
+            this.recalculateLink.AutoSize = true;
+            this.recalculateLink.Location = new System.Drawing.Point(153, 284);
+            this.recalculateLink.Name = "recalculateLink";
+            this.recalculateLink.Size = new System.Drawing.Size(40, 13);
+            this.recalculateLink.TabIndex = 6;
+            this.recalculateLink.TabStop = true;
+            this.recalculateLink.Text = "Reshift";
+            this.recalculateLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.recalculateLink_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 306);
+            this.Controls.Add(this.recalculateLink);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.openLink);
+            this.Controls.Add(this.saveLink);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel saveLink;
+        private System.Windows.Forms.LinkLabel openLink;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.LinkLabel recalculateLink;
+    }
+}
+
---
+++ BackgroundShifter/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BackgroundShifter
+{
+    public partial class MainForm : Form
+    {
+        private List<int> cutPoints = new List<int>();
+        private Bitmap finalBitmap = null;
+        private Bitmap originalBitmap = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = Application.ProductName;
+            DetermineCutPoints();
+        }
+
+        private void openLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog
+            {
+                Filter = "Bitmap files (*.bmp)|*.bmp"
+            };
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Original bitmap
+                originalBitmap = new Bitmap(openDialog.FileName);
+                pictureBox1.Image = originalBitmap;
+
+                pictureBox2.Image = finalBitmap;
+
+                DrawBitmap();
+            }
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            if (finalBitmap != null)
+            {
+                e.Graphics.DrawImage(finalBitmap, e.ClipRectangle);
+            }
+        }
+
+        private void saveLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (finalBitmap != null)
+            {
+                SaveFileDialog saveDialog = new SaveFileDialog
+                {
+                    Filter = "Bitmap files (*.bmp)|*.bmp"
+                };
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                    finalBitmap.Save(saveDialog.FileName);
+            }
+        }
+
+        private void DetermineCutPoints()
+        {
+            cutPoints.Clear();
+            Random randomizer = new Random();
+
+            // Draw five randomly chosen cutpoints
+            for (int i = 0; i < 5; i++)
+            {
+                int number = randomizer.Next(1, 5);
+                cutPoints.Add(number);
+            }
+        }
+
+        private void DrawBitmap()
+        {
+            if (originalBitmap != null)
+            {
+                // Initialize the result bitmap
+                finalBitmap = new Bitmap(originalBitmap.Width * cutPoints.Count, originalBitmap.Height);
+                Graphics grp = Graphics.FromImage(finalBitmap);
+
+                int widthPos = 0;   // Location of next image
+                foreach (int cutPoint in cutPoints)
+                {
+                    // Locations of lowerpart and upperpart in the destination picture
+                    int lowerPart = originalBitmap.Height / cutPoint;
+                    int upperPart = originalBitmap.Height - lowerPart;
+
+                    grp.DrawImage(originalBitmap, new Rectangle(widthPos, 0, originalBitmap.Width, upperPart), new Rectangle(0, lowerPart, originalBitmap.Width, upperPart), GraphicsUnit.Pixel);
+                    grp.DrawImage(originalBitmap, new Rectangle(widthPos, upperPart, originalBitmap.Width, lowerPart), new Rectangle(0, 0, originalBitmap.Width, lowerPart), GraphicsUnit.Pixel);
+
+                    widthPos += originalBitmap.Width;
+                }
+
+                pictureBox2.Invalidate();
+            }
+        }
+
+        private void recalculateLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DetermineCutPoints();
+            DrawBitmap();
+        }
+    }
+}
